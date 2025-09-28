217e4ce3d721412d8009e6a9af7d77586f7d0df5
tanya tomar <tanyatomar@tanyas-MacBook-Air.local>
2025-09-28 23:01:35 +0530

    fix specs

--- spec/lib/string_kata_spec.rb
+++ spec/lib/string_kata_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe StringKata do
   describe 'StringKata' do
     it 'for empty string return zero' do
-      expect(StringKata.add("")).to eq(1)
+      expect(StringKata.add("")).to eq(0)
     end
 
     it 'for empty string return zero' do
-      expect(StringKata.add(nil)).to eq(1)
+      expect(StringKata.add(nil)).to eq(0)
     end
 
     it 'for a single number returns the number' do
@@ -19,11 +19,11 @@ RSpec.describe StringKata do
     end
 
     it 'handles newlines between numbers' do
-      expect(StringKata.add("n2,3")).to eq(6)
+      expect(StringKata.add("1\n2,3")).to eq(6)
     end
 
     it 'handles custom delimiters' do
-      expect(StringKata.add("//;\1;2;4;6")).to eq(13)
+      expect(StringKata.add("//;\n1;2;4;6")).to eq(13)
     end
 
     it 'raises an error when a negative number is included' do
